731bdd6be1375b46f88d33813811cc700a3b3056
david <zollner.david010306@gmail.com>
2024-01-19 15:56:30 +0100

    add create button

--- DesktopLinker/MainWindow.axaml.cs
+++ DesktopLinker/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
+using System;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 
 namespace DesktopLinker;
 
@@ -7,5 +10,10 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void CreateFileBtn_OnClick(object? sender, RoutedEventArgs e)
+    {
+        
     }
 }
